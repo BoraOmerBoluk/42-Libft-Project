4d94c910b032395f7fd958da2010520bee732dcf
Bora Ömer Bölük <68511093+Boraomer@users.noreply.github.com>
2023-03-16 10:25:37 +0300

    Add files via upload

---
+++ bboluk_libft/ft_memchr.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bboluk <bboluk@student.42istanbul.com.t    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/26 21:23:05 by bboluk            #+#    #+#             */
+/*   Updated: 2023/01/26 21:23:05 by bboluk           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t			i;
+	unsigned char	*buffer;
+
+	buffer = (unsigned char *)s;
+	i = 0;
+	while (i < n)
+	{
+		if (*buffer == (unsigned char)c)
+			return (buffer);
+		buffer++;
+		i++;
+	}
+	return (NULL);
+}
---
+++ bboluk_libft/ft_strmapi.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bboluk <bboluk@student.42istanbul.com.t    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/26 21:22:24 by bboluk            #+#    #+#             */
+/*   Updated: 2023/01/26 21:22:24 by bboluk           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	unsigned int	i;
+	char			*str;
+
+	if (!s)
+		return (NULL);
+	i = 0;
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s)) + 1);
+	if (str == NULL)
+		return (NULL);
+	while (s[i] != '\0')
+	{
+		str[i] = f(i, s[i]);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
